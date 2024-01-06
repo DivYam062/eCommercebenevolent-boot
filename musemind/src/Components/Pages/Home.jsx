aea37aaf77699ce73ed925bfdc54ad7f866236b9
DivYam062 <divyamrauthan002@gmail.com>
2024-01-06 09:18:19 +0530

    nav and allrouts

---
+++ musemind/src/Components/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import "./Home.css";
+
+const Home = ()=>{
+    return (
+        <div className="homePage">
+            <div className="bannerDiv">
+                <div className="banner">
+                    <h1>Shopping And <br/> Department Store.</h1>
+                    <p>Shoping is a bit of relexing hobby for me, which <br/> is something troubleing for the bank balance.</p>
+                    <button>Explore</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
